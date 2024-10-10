f0086702ef28ff22bd79b2519cdf37082b2a5a36
whakkhanna <ronabadeous@yahoo.com.ph>
2024-10-10 15:07:49 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,18 +7,5 @@ resource "aws_instance" "nginx" {
   instance_type = "t2.micro"
   tags = {
       Name = "nginx"
-      description = "Nginx server on ubuntu"
   }
-user_data = <<-EOF
-            #!/bin/bash
-            sudo apt update
-            sudo apt install nginx -y
-            systemctl enable nginx
-            systemctl start nginx
-            EOF
-}
-
-output publicip {
-  value   = aws_instance.webserver.public_ip
-}
 
