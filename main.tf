692267dabfe0a8b0fd6b0196f222152e8d759d1e
whakkhanna <ronabadeous@yahoo.com.ph>
2024-10-11 10:00:07 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,23 @@
-provider "aws" {
-    region = "us-east-1"  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
 
-resource "aws_instance" "nginx" {
-  ami           = "ami-0866a3c8686eaeeba"
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+
   tags = {
-      Name = "nginx"
+    Name = "ExampleAppServerInstance"
   }
 }
-
